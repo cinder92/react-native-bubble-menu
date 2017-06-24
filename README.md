0a732222b2b437bb6f0bb739aadb591220e84803
Dante Cervantes <contacto@dantecervantes.com>
2017-06-24 02:26:21 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,42 @@
+# react-native-bubble-menu
+
+A Bubble menu effect for your react native application
+
+# Example
+
+![example](https://github.com/cinder92/react-native-bubble-menu/blob/master/demo.gif)
+
+# How to install
+
+`npm i -S react-native-bubble-menu`
+
+# How to use
+
+```
+import BubbleMenu from 'react-native-bubble-menu';
+
+...
+
+render(){
+   return(
+      <BubbleMenu
+         items={this._renderItems()}
+         closeBtn={this._renderCloseBtn()}
+         show={false}
+		 style={...}
+      />
+   )
+}
+```
+
+# Props
+
+| Name  | Description | Required |
+| ----- | ------------| -------- |
+| show | Set `true` to show and `false` to hide | yes |
+| color | Change color of bubble effect | no |
+| items | Menu items (array) | yes |
+| openBtn | Receives a component for open button | yes |
+| style | Receives a custom style object | no |
+
+# of coooourse PR are welcome :)
---
+++ index.js
@@ -0,0 +1,109 @@
+import React, {Component, PropTypes} from 'react';
+import {
+	View,
+	ScrollView,
+	StyleSheet,
+	Easing,
+	Animated,
+	Dimensions
+} from 'react-native';
+
+const screen = Dimensions.get("window");
+const MENU_WIDTH = screen.width * 0.5;
+const MENU_HEIGHT = screen.height;
+
+class BubbleMenu extends Component{
+
+	constructor(props){
+		super(props);
+
+		this.state = {
+			openMenu : new Animated.Value(50),
+			opacity : new Animated.Value(0)
+		}
+	}
+
+	componentWillReceiveProps(nextState){
+		const {show} = nextState;
+
+		let toValue = show ? screen.width - 25 : 50;
+
+		Animated.parallel([
+			Animated[show ? "spring" : "timing"](
+				this.state.openMenu,{
+					toValue,
+					delay : show ? 0 : 150,
+					duration : 250,
+					easing : Easing.inOut(Easing.ease)
+				}
+			),
+			Animated.timing(
+				this.state.opacity,{
+					toValue : show ? 1 : 0,
+					delay : show ? 150 : 0,
+					duration : 150,
+					easing : Easing.inOut(Easing.ease)
+				}
+			)
+		]).start();
+
+
+	}
+
+	render(){
+		const {show, items, openBtn, color, style} = this.props;
+		return(
+			<Animated.View style={[styles.container,{
+				width : this.state.openMenu,
+				backgroundColor : show ? (color) ? color : "#ffffff" : (color) ? color : "#fafafa"
+			},style]}>
+
+					<View
+						style={styles.button}
+						pointerEvents={show ? "none" : "auto" }>
+						{openBtn}
+					</View>
+
+					<Animated.View style={[styles.items,{
+						opacity : this.state.opacity
+					}]} pointerEvents={show ? "auto" : "none" }>
+						{items}
+					</Animated.View>
+
+			</Animated.View>
+		)
+	}
+}
+
+BubbleMenu.propTypes = {
+	color : PropTypes.string,
+	show : PropTypes.bool.isRequired,
+	items : PropTypes.array.isRequired,
+	openBtn : PropTypes.object.isRequired,
+	style : PropTypes.number
+}
+
+const styles = StyleSheet.create({
+	container : {
+		justifyContent: 'center',
+		alignItems: 'center',
+		borderRadius : 50 / 2
+	},
+
+	button : {
+		width : 50,
+		height : 50,
+		borderRadius : 50 / 2,
+		justifyContent: 'center',
+		alignItems: 'center'
+	},
+
+	items : {
+		flexDirection : "row",
+		alignItems : "center",
+		justifyContent : "space-between",
+		marginTop : -50
+	}
+});
+
+export default BubbleMenu;
